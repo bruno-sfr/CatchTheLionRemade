92dec87a2b43ca8f32465da787380d130575261d
bruno-sfr <brunoschaffer24@gmail.com>
2024-03-18 16:08:30 +0100

    it doesnt matter

--- score_calc.sh
+++ score_calc.sh
@@ -3,7 +3,7 @@
 total_score_x=0
 total_score_y=0
 
-for file in /home/bruno.schaffer/CatchTheLionRemade/Resources/MCTS_MR-vs-Alpha_Beta_Fix/4-50-MCTS_MR-4-0-vs-Alpha_Beta_Fix-5-0/*.txt; do
+for file in /home/bruno.schaffer/Resources/Alpha_Beta_Fix-vs-Alpha_Beta_Fix/4-0-Alpha_Beta_Fix-8-0-vs-Alpha_Beta_Fix-4-0/*.txt; do
     scores=$(head -n 1 "$file" | grep -oE '[0-9]+(\.[0-9]+)?,[0-9]+(\.[0-9]+)?|[0-9]+(\.[0-9]+)?:[0-9]+(\.[0-9]+)?' | tr '\n' ' ')
     score_x=$(echo "$scores" | awk -F: '{print $1}' | awk -F',' '{print $1}')
     score_y=$(echo "$scores" | awk -F: '{print $2}' | awk -F',' '{print $1}')
