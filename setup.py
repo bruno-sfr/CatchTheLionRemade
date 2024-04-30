0a25e372be7b754ffed005211c2ed18b91f0d9b2
bruno-sfr <brunoschaffer24@gmail.com>
2024-04-30 14:27:04 +0200

    added setup to project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='CatchTheLionRemade',
+    version='1.0',
+    packages=['AlphaBeta', 'Game', 'GUI', 'MonteCarlo'],
+    package_dir={'': '.'},
+    url='https://github.com/bruno-sfr/CatchTheLionRemade/tree/main',
+    license='',
+    author='brunoschaffer',
+    author_email='brunoschaffer24@gmail.com',
+    description=''
+)
